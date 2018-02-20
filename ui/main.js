364c09f99a59203bb5490c042fd521b7cbc2eeb1
iamuv10 <36511334+iamuv10@users.noreply.github.com>
2018-02-20 21:32:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,21 @@
-console.log('Loaded!');
-//lchange the content of the main text
-
-var element = document.getElementById("main-text");
-element.innerHTML = "New Value";
-
-//Move the image
-
-var img = document.getElementById("madi");
-var marginleft = 0;
-function moveRight(){
-    marginleft = marginleft + 5;
-    img.style.marginLeft = marginleft + "px";
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+//Counter code
+var button = document.getElementById('counter');
+button.onclick = function(){
+  //Create a request to counter endpoint
+  var request = new XMLHTTPrequest();
+  //Capture the response and store it in a variabe
+  request.onreadystatechange = function(){
+    if (request.readyState == XMLHTTPrequest.DONE){
+        //Take some action
+        if (request.status == 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+    //Not done yet
+  };
+  //Make a request
+  request.open("GET","http://yuvisc7.imad-app.hasura.io/counter",true);
+  request.send(null);
 };
