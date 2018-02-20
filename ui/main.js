45132a8f234b59d128e57586f3d7941ff350069a
iamuv10 <36511334+iamuv10@users.noreply.github.com>
2018-02-20 08:58:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var img = document.getElementById("madi");
 var marginleft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginleft + "px";
+    img.style.marginLeft = marginLeft + "px";
 }
 img.onclick = function(){
     var interval = setInterval(moveRight,100);
