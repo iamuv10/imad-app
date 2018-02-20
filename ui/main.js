c7b5c5264fbeadadb595f3de671b2cce1581d8d7
iamuv10 <36511334+iamuv10@users.noreply.github.com>
2018-02-20 08:51:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
+
+//Move the image
+
+var img = document.getElementById("madi");
+img.onclick = function(){
+    img.style.marginleft = "100px";
+};
