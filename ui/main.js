da00a72060de356001de0445f297b2019e509de2
iamuv10 <36511334+iamuv10@users.noreply.github.com>
2018-02-20 08:44:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 //lchange the content of the main text
 
-var element = document.getElementbyId["main-text"];
+var element = document.getElementById["main-text"];
 element.innerHTML = "New Value";
